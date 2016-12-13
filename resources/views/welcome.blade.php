280e16f93c5ce53b095dde51d9c5a4c5ef2aa9aa
Stephen <SiNuAge@gmail.com>
2016-12-13 01:05:34 -0500

    Basic changes
    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Stephen's Temporary page</title>
+        <title>Stephen's Laravel page</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -82,7 +82,7 @@
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://forge.laravel.com">Forge <- not really using this</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
